a19e98c0613fa5c590a05afaf69f1457c3b8df2d
doojoo1221 <parkbr95@naver.com>
2021-12-30 16:52:25 +0900

    modified timezone, language_code

--- outmoongram2/outmoongram2/settings.py
+++ outmoongram2/outmoongram2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0$d%(h15fsa-sp5q3mz)83s#)+r+@pl#8wrddtbz=m+bc6jyx!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'asia/seoul'
 
 USE_I18N = True
 
