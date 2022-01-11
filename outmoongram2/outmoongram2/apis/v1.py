16e8bc588d41290b6b0f4018aed38e1f31526855
doojoo1221 <parkbr95@naver.com>
2022-01-11 20:30:19 +0900

    로그인 api 추가

--- outmoongram2/outmoongram2/apis/v1.py
+++ outmoongram2/outmoongram2/apis/v1.py
@@ -5,6 +5,7 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.core.validators import validate_email, ValidationError
+from django.contrib.auth import authenticate, login, logout
 
 class BaseView(View):
     @staticmethod
@@ -15,6 +16,20 @@ class BaseView(View):
         }
         return JsonResponse(result, status=status)
 
+class UserLoginView(BaseView):
+    def post(self,request):
+        username = request.POST.get('username', '')
+        if not username:
+            return self.response(message='아이디를 입력해 주세요.', status=400)
+        password = request.POST.get('password', '')
+        if not password:
+            return self.response(message='비패스워드를 입력해 주세요.', status=400)
+
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            return self.response(message='입력 정보를 확인해 주세요', status=400)
+        login(request, user)
+        return self.response()
 
 class UserCreateView(BaseView):
     @method_decorator(csrf_exempt)
@@ -44,3 +59,4 @@ class UserCreateView(BaseView):
             return self.response(message='이미 존재하는 아이디입니다.', status=400)
 
         return self.response({'user.id': user.id})
+
